a6b6cc9a83baf99290993077df841e9df9f94325
Shaik Mohammed Zakir <smzakir123@gmail.com>
2026-06-02 12:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+
+      const height = document.getElementById("height");
+      const width = document.getElementById("width");
+
+      function updateDimensions() {
+        width.innerText = window.innerWidth;
+        height.innerText = window.innerHeight;
+      }
+      updateDimensions();
+      window.addEventListener("resize", function () {
+        updateDimensions();
+      });//your JS code here. If required.
+    <script>
+      const height = document.getElementById("height");
+      const width = document.getElementById("width");
+
+      function updateDimensions() {
+        width.innerText = window.innerWidth;
+        height.innerText = window.innerHeight;
+      }
+      updateDimensions();
+      window.addEventListener("resize", function () {
+        updateDimensions();
+      });
